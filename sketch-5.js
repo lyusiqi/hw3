f02373240c029a15e6369893fea4c2f72df5ef64
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-25 16:04:04 -0700

    Create sketch-5.js

---
+++ sketch-5.js
@@ -0,0 +1,24 @@
+background(255);
+noStroke();
+
+
+var h = 340;
+
+while(true) {
+  background(255);
+  colorMode(HSB);
+  fill(h, 100, 100);
+  ellipse(width=h, 200, 40, 40);
+  h = h-10;
+  
+  if (h >= 340) {
+    
+    while(true) {
+  background(255);
+  colorMode(HSB);
+  fill(h, 100, 100);
+  ellipse(width=h, 200, 40, 40);
+   h=h+10
+    }
+  }
+}
