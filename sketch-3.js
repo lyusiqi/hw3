ed3105d1cbe5a1c4f4a1ebd3e8d35cbc577bda64
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-25 15:40:15 -0700

    Update sketch-3.js

--- sketch-3.js
+++ sketch-3.js
@@ -2,14 +2,14 @@
 background(255);
 noStroke();
 
-var h = 0;
+var h = 100;
 
 while(true) {
   background(255);
   colorMode(HSB);
   fill(h, 100, 100);
-  ellipse(width=h, height=h, 40, 40);
-  h = h+30;
+  ellipse(width=h, 200, 40, 40);
+  h = h+10;
   
   if (h >= 340) {
     h = 0;
